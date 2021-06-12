038032a37549c7310953b6d9f65df8fea49a38ff
caropolis1972 <48814954+caropolis1972@users.noreply.github.com>
2021-06-12 12:34:03 -0500

    Added username & role to the authenticate endpoint response.

--- src/main/java/com/meritamerica/capstonebackend/models/AuthenticationResponse.java
+++ src/main/java/com/meritamerica/capstonebackend/models/AuthenticationResponse.java
@@ -1,15 +1,29 @@
 package com.meritamerica.capstonebackend.models;
 
 public class AuthenticationResponse {
-	
+	private final String username;
+	private final String role;
 	private final String jwt;
 	
-	public AuthenticationResponse(String jwt) {
+	public AuthenticationResponse(String username, String role, String jwt) {
+		this.username = username;
 		this.jwt = jwt;
+		
+		if (role != null && role.startsWith("ROLE_")) {
+			role = role.substring(5);
+		}
+		this.role = role;
+	}
+	
+	public String getUsername() {
+		return this.username;
+	}
+
+	public String getRole() {
+		return this.role;
 	}
 
 	public String getJwt() {
-		return jwt;
-	}
-	
+		return this.jwt;
+	}	
 }
