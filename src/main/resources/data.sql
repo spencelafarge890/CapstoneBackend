d29a4b298ca086301061be67d899d74e7f92f2ec
caropolis1972 <48814954+caropolis1972@users.noreply.github.com>
2021-06-13 12:41:13 -0500

    Added sample CD offering records.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,8 @@
 
 INSERT INTO merit_bank_user (username, password, role) VALUES ('admin', 'admin', 'ROLE_ADMIN');
+INSERT INTO cdoffering (id, interest_rate, term) VALUES (1, 0.1, 3);
+INSERT INTO cdoffering (id, interest_rate, term) VALUES (2, 0.2, 6);
+INSERT INTO cdoffering (id, interest_rate, term) VALUES (3, 0.33, 12);
 INSERT INTO account_holder (combined_balance, first_name, last_name, middle_name, ssn, user_id) VALUES ('1000', 'admin', 'mcadmin', 'd', '123456789', '1');
 INSERT INTO savings_account (account_open_date, balance, interest_rate, account_holder_id) VALUES (now(), '1000', '0.1', '1');
 INSERT INTO transaction (transaction, amount, origin, transaction_date, account, transaction_type) VALUES ('deposit', '100', 'ATM', now(), '1', 'deposit');
