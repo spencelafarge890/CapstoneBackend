70743061f270d4e1e87cb07a579376bc65f610d8
caropolis1972 <48814954+caropolis1972@users.noreply.github.com>
2021-06-20 14:56:56 -0500

    Added endpoint to delete user.

--- src/main/java/com/meritamerica/capstonebackend/controllers/MeritBankController.java
+++ src/main/java/com/meritamerica/capstonebackend/controllers/MeritBankController.java
@@ -9,20 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.*;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.meritamerica.capstonebackend.models.AccountHolder;
 import com.meritamerica.capstonebackend.models.AuthenticationRequest;
@@ -72,6 +61,16 @@ public class MeritBankController {
 	@ResponseStatus(HttpStatus.OK)
 	public List<MeritBankUser> getAllUsers() {
 		return meritUserDetailService.getAllUsers();
+	}
+	
+	@DeleteMapping(value = "/users/{id}")
+    public ResponseEntity<Integer> deleteUser(@PathVariable Integer id) {
+		boolean isDeleted = meritUserDetailService.deleteById(id);
+		if (isDeleted) {
+			return new ResponseEntity<>(id, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);			
+		}
 	}
 	
 	@ResponseStatus(HttpStatus.OK)
@@ -250,7 +249,4 @@ public class MeritBankController {
 		accountHolderSvc.deleteCDAccount(cdAccount, accHolder);
 		return cdAccount;
 	}
-	
-	
-
 }
